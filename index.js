04cdf0c6de13a2c0666d53ea1f5f9fd7c419e928
Alex Vasiuk <alexvasiuk@gmail.com>
2022-08-09 09:06:21 +0200

    Add fetching DLC data

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// import Web3 from './web3';
-
 async function loadWeb3() {
     if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
@@ -9,15 +7,34 @@ async function loadWeb3() {
 
 async function load() {
     await loadWeb3();
-    window.contract = await loadContract();
+    window.DLCContract = await loadDLCContract();
     window.hotDogsContract = await loadHotDogContract();
     await configureHotDogHooks(window.hotDogsContract);
+    await updateDLCStatus()
     updateHotDogs('Ready!');
 }
 
 function updateHotDogs(status) {
     const hotDogsStatus = document.getElementById('hotDogsStatus');
     hotDogsStatus.innerHTML = status;
+    console.log(status);
+}
+
+function updateDLCTotal(status) {
+    const DLCTotalStatus = document.getElementById('DLCTotalStatus');
+    DLCTotalStatus.innerHTML = status;
+    console.log(status);
+}
+
+function updateDLCOpen(status) {
+    const DLCOpenStatus = document.getElementById('DLCOpenStatus');
+    DLCOpenStatus.innerHTML = status;
+    console.log(status);
+}
+
+function updateDLCAge(status) {
+    const DLCAgeStatus = document.getElementById('DLCAgeStatus');
+    DLCAgeStatus.innerHTML = status;
     console.log(status);
 }
 
@@ -33,9 +50,9 @@ async function configureHotDogHooks(contract) {
         .on('data', event => getHotDogsNumber())
 }
 
-async function loadContract() {
+async function loadDLCContract() {
     var ABI = await fetch('./contract_abi.json').then(res => res.json())
-    const contractAddress = "0x365441EC0974F6AC9871c704128e9da2BEdE10CE"
+    const contractAddress = "0x6143a931d4bCB38347936fAacFbea432b4F0DF17"
     return await new window.web3.eth.Contract(ABI, contractAddress);
 }
 
@@ -43,6 +60,19 @@ async function loadHotDogContract() {
     var ABI = await fetch('./hot_dog_contract_abi.json').then(res => res.json())
     const contractAddress = "0xD2ce3b3da81095cBcB0761EBd5029c8499221C0F"
     return await new window.web3.eth.Contract(ABI, contractAddress);
+}
+
+async function updateDLCStatus() {
+    const dlcs = await getDLCs()
+    await updateDLCTotal(dlcs.length)
+    await updateDLCOpen(dlcs.filter(dlc => dlc.actualClosingTime == "0").length)
+}
+
+async function getDLCs() {
+    const uuids = await window.DLCContract.methods.allOpenDLC().call()
+    return Promise.all(
+        uuids.map(
+            uuid => window.DLCContract.methods.dlcs(uuid).call()))
 }
 
 async function getHotDogsNumber() {
